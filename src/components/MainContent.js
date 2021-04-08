86bdbb87cc000b1792a5a8b8b0e1920b456eab19
ptwob400 <ptwob400@gmail.com>
2021-04-08 16:08:52 -0500

    loading bar on fields

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,14 +2,34 @@ import Top10Keywords from './Top10Keywords.js'
 import Top10Locations from './Top10Locations.js'
 import Top10Fields from './Top10Fields.js'
 import DropDownMenu from './DropDownMenu.js'
+import { Grid, Paper } from '@material-ui/core';
+import { useState } from 'react';
 
 function MainContent() {
     return (<body className="Body">
         <section>
-            <DropDownMenu />
-            <Top10Keywords />
-            <Top10Locations />
-            <Top10Fields />
+            <Grid container justify='center'>
+                <Grid item>
+                    <DropDownMenu />
+                </Grid>
+            </Grid>
+            <Grid container justify='center'>
+                <Grid item>
+                    <Paper style={{ padding: 10, margin: 15}}>
+                    <Top10Keywords />
+                    </Paper>
+                </Grid>
+                <Grid item>
+                <Paper style={{ padding: 10, margin: 15}}>
+                    <Top10Locations />
+                    </Paper>
+                </Grid>
+                <Grid item>
+                <Paper style={{ padding: 10, margin: 15}}>
+                    <Top10Fields />
+                    </Paper>
+                </Grid>
+            </Grid>
         </section>
     </body>)
 }
